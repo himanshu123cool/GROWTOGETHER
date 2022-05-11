32048c51b28a9e3acc09a6d8db19a7bd1d17da1a
himanshucool123 <himanshu312cool@gmail.com>
2022-05-11 17:05:21 +0530

    finish

---
+++ portfolio/script.js
@@ -0,0 +1,77 @@
+const _open = document.getElementById('open');
+const _close = document.getElementById('close');
+const _sidebar = document.getElementById('sidebar');
+const _navbar = document.getElementById('navbar');
+const _nav_links = document.querySelector('.nav-links');
+
+
+function op(){
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    console.log(width);
+    if(width>=0 && width<=319){
+        _sidebar.style.width='100%';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=250 && width<=480){
+        _sidebar.style.width='100%';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=481 && width<=768){
+        _sidebar.style.width='100%';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=769 && width<=1024){
+        _sidebar.style.width='30vw';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=1025 && width<=1200){
+        _sidebar.style.width='23vw';
+        _nav_links.style.visibility='visible';
+    }
+    else{
+        _sidebar.style.width='18vw';
+        _nav_links.style.visibility='visible';
+    }    
+}
+
+function cl(){
+    _sidebar.style.width='0';
+    _nav_links.style.visibility='hidden';
+}
+
+window.onscroll = function navchange(){
+    if(document.body.scrollTop>250 || document.documentElement.scrollTop>250){
+        _navbar.style.backgroundColor='var(--dark-color)';
+    } 
+    else{
+        _navbar.style.backgroundColor='';
+    }
+}
+
+
+
+
+
+
+
+
+
+_portfolioImg = document.querySelectorAll('.portfolio-img');
+_portfolioOverlay = document.querySelector('.portfolio-overlay');
+_overlayImg = document.querySelector('.overlay-img');
+_overlayClose = document.getElementById('overlay-close');
+
+
+_portfolioImg.forEach(image =>{
+    image.onclick = () =>{
+        _portfolioOverlay.style.visibility='visible'
+        _overlayImg.src = image.getAttribute('src')
+    }
+})
+
+function overlay(){
+    _portfolioOverlay.style.visibility='hidden'
+}
+
+
+
---
+++ price/script.js
@@ -0,0 +1,85 @@
+const _open = document.getElementById('open');
+const _close = document.getElementById('close');
+const _sidebar = document.getElementById('sidebar');
+const _navbar = document.getElementById('navbar');
+const _nav_links = document.querySelector('.nav-links');
+
+
+function op(){
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    console.log(width);
+    if(width>=0 && width<=319){
+        _sidebar.style.width='100%';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=250 && width<=480){
+        _sidebar.style.width='100%';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=481 && width<=768){
+        _sidebar.style.width='100%';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=769 && width<=1024){
+        _sidebar.style.width='30vw';
+        _nav_links.style.visibility='visible';
+    }
+    else if(width>=1025 && width<=1200){
+        _sidebar.style.width='23vw';
+        _nav_links.style.visibility='visible';
+    }
+    else{
+        _sidebar.style.width='18vw';
+        _nav_links.style.visibility='visible';
+    }    
+}
+
+function cl(){
+    _sidebar.style.width='0';
+    _nav_links.style.visibility='hidden';
+}
+
+window.onscroll = function navchange(){
+    if(document.body.scrollTop>250 || document.documentElement.scrollTop>250){
+        _navbar.style.backgroundColor='var(--dark-color)';
+    } 
+    else{
+        _navbar.style.backgroundColor='';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('.price-content-box-annually').hide()
+
+    $('.annual-btn').click(function(){
+        $('.price-content-box-annually').show()
+        $('.price-content-box-quarterly').hide()
+        $('.annual-btn').addClass('tab-active')
+        $('.quarter-btn').removeClass('tab-active')
+    })
+
+    $('.quarter-btn').click(function(){
+        $('.price-content-box-annually').hide()
+        $('.price-content-box-quarterly').show()
+        $('.annual-btn').removeClass('tab-active')
+        $('.quarter-btn').addClass('tab-active')
+    })
+})
