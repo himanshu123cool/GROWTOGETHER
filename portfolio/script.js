5a2d6c3fc7213ccd0f2718da77d164cb66a66e1d
himanshucool123 <himanshu312cool@gmail.com>
2022-09-22 16:39:43 +0530

    finish

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,38 +1,44 @@
-$("#open").click(function () {
-  $("#close").show(300);
-  $("#open").hide(300);
-  var width = window.innerWidth > 0 ? window.innerWidth : window.screen;
-  if (width >= 0 && width <= 319) {
-    $("#sidebar").css({ width: "100%" });
-    $(".side_menu_list").css({ visibility: "visible" });
-  } else if (width >= 319 && width <= 480) {
-    $("#sidebar").css({ width: "100%" });
-    $(".side_menu_list").css({ visibility: "visible" });
-  } else if (width >= 481 && width <= 768) {
-    $("#sidebar").css({ width: "100%" });
-    $(".side_menu_list").css({ visibility: "visible" });
-  } else if (width >= 769 && width <= 1024) {
-    $("#sidebar").css({ width: "50%" });
-    $(".side_menu_list").css({ visibility: "visible" });
-  } else {
-    $("#sidebar").css({ width: "0" });
+$(document).ready(function () {
+  $("#open").click(function () {
+    $("#close").show(300);
+    $("#open").hide(300);
+    var width = window.innerWidth > 0 ? window.innerWidth : window.screen;
+    if (width >= 0 && width <= 319) {
+      $("#sidebar").css({ width: "100%" });
+      $(".side_menu_list").css({ visibility: "visible" });
+    } else if (width >= 319 && width <= 480) {
+      $("#sidebar").css({ width: "100%" });
+      $(".side_menu_list").css({ visibility: "visible" });
+    } else if (width >= 481 && width <= 768) {
+      $("#sidebar").css({ width: "100%" });
+      $(".side_menu_list").css({ visibility: "visible" });
+    } else if (width >= 769 && width <= 1024) {
+      $("#sidebar").css({ width: "50%" });
+      $(".side_menu_list").css({ visibility: "visible" });
+    } else {
+      $("#sidebar").css({ width: "0" });
+      $(".side_menu_list").css({ visibility: "hidden" });
+    }
+  });
+});
+
+$(document).ready(function () {
+  $("#close").click(function () {
+    $("#close").hide(300);
+    $("#open").show(300);
+    $("#sidebar").css({ width: "0%" });
     $(".side_menu_list").css({ visibility: "hidden" });
-  }
+  });
 });
 
-$("#close").click(function () {
-  $("#close").hide(300);
-  $("#open").show(300);
-  $("#sidebar").css({ width: "0%" });
-  $(".side_menu_list").css({ visibility: "hidden" });
-});
-
-$(window).scroll(function () {
-  if (window.scrollY > 250) {
-    $("#wrapper").css({ backgroundColor: "var(--navscroll-color)" });
-  } else {
-    $("#wrapper").css({ backgroundColor: "var(--dark-color)" });
-  }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (window.scrollY > 250) {
+      $("#wrapper").css({ backgroundColor: "var(--navscroll-color)" });
+    } else {
+      $("#wrapper").css({ backgroundColor: "var(--dark-color)" });
+    }
+  });
 });
 
 _portfolioImg = document.querySelectorAll(".portfolio-img");
